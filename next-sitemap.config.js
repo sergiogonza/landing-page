0df688a16188084555a0ab10876d4a0e034afd31
sergiogonza <sergiogonzawr@gmail.com>
2025-10-24 14:48:47 -0500

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://landingpage.huglemon.com',
+  siteUrl: process.env.SITE_URL || 'https://hostly.io',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude: ['/404', '/500'],
